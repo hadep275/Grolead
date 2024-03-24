1ab673f618bc9fa6fe77b63b54080709d2c2e7e1
hadep275 <hadep275@gmail.com>
2024-03-24 14:30:06 -0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import React, { useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ProductListing from "./components/ProductListing";
-import ProductDetails from "./components/ProductDetails";
+import ErrorPage from "./pages/ErrorPage";
 import Home from "./pages/Home";
+import Navigation from "./components/Navigation";
+import ProductDetails from "./components/ProductDetails";
+import ProductListing from "./components/ProductListing";
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
-  
   return (
     <>
-    <Home/>
+    <Navigation/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<ProductListing />} />
         <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
   );
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import '../css/Navigation.css'
 const Navigation = () => {
   return (
     <div>
