5eaf1c9ebc08eb0db9ea3cd04b502ae9ed7cacf1
hadep275 <hadep275@gmail.com>
2024-03-29 19:05:05 -0600

    navbar updated

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "../css/Navigation.css";
 import "../css/root.css";
 const Navigation = () => {
@@ -40,95 +40,99 @@ const Navigation = () => {
   }, []);
 
   return (
-    <header
-      className={`header ${isNavOpen ? "active" : ""} ${
-        isScrolled ? "scrolled" : ""
-      }`}
-    >
-      <div className="container">
-        <a href="#" className="logo">
-          <img src="logo.svg" width="40" height="5" alt="logo" />
-        </a>
+    <>
+      <header
+        className={`header ${isNavOpen ? "active" : ""} ${
+          isScrolled ? "scrolled" : ""
+        }`}
+      >
+          
 
-        <nav className={`navbar ${isNavOpen ? "active" : ""}`}>
-          <ul className="navbar-list">
-            <li>
-              <Link
-                to="/"
-                className="navbar-link hover-1"
-                onClick={toggleNav}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/products"
-                className="navbar-link hover-1"
-                onClick={toggleNav}
-              >
-                Shop
-              </Link>
-            </li>
-            <li>
-              <a
-                href="#blog"
-                className="navbar-link hover-1"
-                onClick={toggleNav}
-              >
-                Blog
-              </a>
-            </li>
-            <li>
-              <a
-                href="#contact"
-                className="navbar-link hover-1"
-                onClick={toggleNav}
-              >
-                Contact
-              </a>
-            </li>
-            <li>
-              <a
-                href="#login"
-                className="navbar-link hover-1"
-                onClick={toggleNav}
-              >
-                Login
-              </a>
-            </li>
-            <li>
-              <a
-                href="#cart"
-                className="navbar-link hover-1"
-                onClick={toggleNav}
-              >
-                Cart
-                <span className="cart-count">0</span>{" "}
-                {/* You can dynamically update this with the actual count */}
-              </a>
-            </li>
-          </ul>
-        </nav>
+          <nav className={`navbar ${isNavOpen ? "active" : ""}`}>
+        <div className="container">
 
-        {/* Nav toggler button */}
-        <button
-          className="nav-open-btn"
-          aria-label="open menu"
-          onClick={toggleNav}
-        >
-          <ion-icon name="menu-outline" aria-hidden="true"></ion-icon>
-        </button>
+           <a href="#" className="logo">
+            <img src="logo.svg" width="40" height="5" alt="logo" />
+          </a> 
+            <ul className="navbar-list">
+              <li>
+                <Link
+                  to="/"
+                  className="navbar-link hover-1"
+                  onClick={toggleNav}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/products"
+                  className="navbar-link hover-1"
+                  onClick={toggleNav}
+                >
+                  Shop
+                </Link>
+              </li>
+              <li>
+                <a
+                  href="#blog"
+                  className="navbar-link hover-1"
+                  onClick={toggleNav}
+                >
+                  Blog
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#contact"
+                  className="navbar-link hover-1"
+                  onClick={toggleNav}
+                >
+                  Contact
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#login"
+                  className="navbar-link hover-1"
+                  onClick={toggleNav}
+                >
+                  Login
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#cart"
+                  className="navbar-link hover-1"
+                  onClick={toggleNav}
+                >
+                  Cart
+                  <span className="cart-count">0</span>{" "}
+                  {/* You can dynamically update this with the actual count */}
+                </a>
+              </li>
+            </ul>
 
-        {/* Scroll up button */}
-        <a
-          href="#top"
-          className="back-top-btn"
-          aria-label="back to top"
-          data-back-top-btn
-        ></a>
-      </div>
-    </header>
+          
+        </div>
+            </nav>
+            {/* Nav toggler button */}
+          <button
+            className="nav-open-btn"
+            aria-label="open menu"
+            onClick={toggleNav}
+            >
+            <ion-icon name="menu-outline" aria-hidden="true"></ion-icon>
+          </button>
+      </header>
+      {/* Scroll up button */}
+      <a
+        href="#top"
+        className="back-top-btn"
+        aria-label="back to top"
+        data-back-top-btn
+      ></a>
+    </>
   );
 };
 
